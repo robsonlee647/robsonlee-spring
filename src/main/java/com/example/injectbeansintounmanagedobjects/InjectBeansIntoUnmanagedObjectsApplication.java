f8994d3912715a0e8369daaa76788343875a3699
Robson Lee <97758663+robsonlee647@users.noreply.github.com>
2024-03-11 07:46:51 -0400

    adding new project

---
+++ src/main/java/com/example/injectbeansintounmanagedobjects/InjectBeansIntoUnmanagedObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.injectbeansintounmanagedobjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectBeansIntoUnmanagedObjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectBeansIntoUnmanagedObjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/injectbeansintounmanagedobjects/InjectBeansIntoUnmanagedObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.injectbeansintounmanagedobjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectBeansIntoUnmanagedObjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
